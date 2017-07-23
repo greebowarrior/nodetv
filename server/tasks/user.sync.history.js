eef96cd9fdc9319628829b866ab090f8506202a6
Martin Purcell <martin.purcell@gmail.com>
2017-07-23 21:11:55 +0100

    Improved user history sync task. Tidied show views

--- server/tasks/user.sync.history.js
+++ server/tasks/user.sync.history.js
@@ -2,10 +2,6 @@
 
 // Sync user watch history
 
-// Is this causing CPU spikes and the non-responsiveness?
-// limit to changes in the past 60 mins
-
-
 const helpers = require('nodetv-helpers')
 
 const User = helpers.model('user')
@@ -14,40 +10,49 @@ const Show = helpers.model('show')
 require('node-schedule').scheduleJob('0,30 * * * *', function(){	
 	console.debug('Syncing watch history with Trakt.tv')
 	
-//	let since = new Date()
-//	since.setDays(since.getDays()-14)
+	let since = new Date()
+	since.setDate(since.getDate()-7)
 	
-	User.find({trakt:{$exists:true}})
-		.then(users=>{
-			if (!users) throw new Error('No users are connected to Trakt.tv')
-			
-			users.forEach(user=>{
-				
-				helpers.trakt(user).sync.history.get({type:'shows'})
-					.then(history=>{
-						history.forEach(item=>{
-							if (item.episode){
-								Show.findByTrakt(item.show.ids.trakt)
-									.then(show=>{
-										return show.getEpisode(item.episode.season, item.episode.number)
-											.then(episode=>{
-												return episode.setWatched(user, item.watched_at, item.id)
-											})
-											.then(()=>{
-												return show.save()
-											})
-									})
-									.catch(error=>{
-										console.debug(error)
-									})
+	User.find({trakt:{$exists:true}}).exec()
+		.map(user=>{
+			helpers.trakt(user).sync.history.get({type:'shows', start_at:since})
+				.then(watches=>{
+					let history = []
+					
+					watches.forEach(item=>{
+						if (item.episode){
+							let idx = history.findIndex(show=>{
+								return show.trakt == item.show.ids.trakt
+							})
+							if (idx >= 0){
+								history[idx].history.push(item)
+							} else {
+								history.push({
+									show: item.show.title,
+									trakt: item.show.ids.trakt,
+									history: [item]
+								})
 							}
-						})
-					})
-					.catch(error=>{
-						console.debug(error)
+						}
 					})
 					
-			})
+					history.forEach(item=>{
+						Show.findByTrakt(item.trakt)
+							.then(show=>{
+								if (!show) return
+								item.history.forEach(watch=>{
+									show.getEpisode(watch.episode.season, watch.episode.number)
+										.then(episode=>{
+											return episode.setWatched(user, watch.watched_at, watch.id)
+										})
+								})
+								return show.save()
+							})
+					})
+				})
+				.catch(error=>{
+					console.debug(error)
+				})
 		})
 		.catch(error=>{
 			console.debug(error.message)
