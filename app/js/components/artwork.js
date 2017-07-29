f40f648a954acb1b0cdb3c1cb10d3ac485a714c9
Martin Purcell <martin.purcell@gmail.com>
2017-07-29 16:52:10 +0100

    Create multiple sizes for artwork to use in srcset. Also has the bonus side-effect of compressing the images :)

--- app/js/components/artwork.js
+++ app/js/components/artwork.js
@@ -3,6 +3,7 @@
 angular.module('nutv.core')
 	
 	.component('showArtwork', {
+		// Component for downloading artwork
 		templateUrl: '/views/components/artwork.html',
 		bindings: {
 			show: '=',
@@ -11,7 +12,7 @@ angular.module('nutv.core')
 		},
 		controller: ['$http','$log','alertService', function($http,$log,alertService){
 			this.save = ()=>{
-				$http.post(`/api/shows/${this.show.ids.slug}/artwork`, {url:this.artwork.url,type:this.type})
+				$http.post(`/api/shows/${this.show.ids.slug}/artwork`, {preview:this.artwork.preview,url:this.artwork.url,type:this.type})
 					.then(res=>{
 						alertService.notify({type:'success',msg:'Artwork saved'})
 						$log.debug(res.data)
@@ -24,11 +25,31 @@ angular.module('nutv.core')
 		}]
 	})
 	
+	.component('nutvBanner', {
+		templateUrl: '/views/components/banner.html',
+		bindings: {item:'<', type:'@'},
+		controller: ['$log','$timeout',function($log,$timeout){
+			$timeout(()=>{
+				let sources = []
+				let directory = this.item.config.directory.replace(/\s/g,'%20')
+				
+				if (this.item.images.banner.files.length){
+					this.item.images.banner.files.forEach(file=>{
+						sources.push(`/media/${this.type}s/${directory}/${file.filename} ${file.width}w`)
+					})
+				} else if (this.item.images.banner.filename){
+					sources.push(`/media/${this.type}s/${directory}/${this.item.images.banner.filename} 800w`)
+				}
+				this.srcset = sources.join(', ')
+			},0)
+		}]
+	})
+	
 	.component('nutvArtwork', {
 		// component for displaying artwork
 		templateUrl: '/views/components/poster.html',
 		bindings: {item:'<', type:'<'},
-		controller: ['$timeout',function($timeout){
+		controller: ['$log','$timeout',function($log,$timeout){
 			let sources = []
 			this.title = {banner:false,poster:false,text:null}
 			
@@ -36,17 +57,29 @@ angular.module('nutv.core')
 				let directory = this.item.config.directory.replace(/\s/g,'%20')
 				
 				if (this.item.images.banner.enabled){
-					sources.push(`/media/${this.type}s/${directory}/${this.item.images.banner.filename} 992w`)
+					if (this.item.images.banner.files.length){
+						this.item.images.banner.files.forEach(file=>{
+							sources.push(`/media/${this.type}s/${directory}/${file.filename} ${file.width}w`)
+						})
+					} else {
+						sources.push(`/media/${this.type}s/${directory}/${this.item.images.banner.filename} 800w`)
+					}
 				} else {
-					sources.push(`/static/gfx/default-banner.png 992w`)
+					sources.push(`/static/gfx/default-banner.png 800w`)
 					this.title.text = this.item.title
 					this.title.banner = true
 				}
 				
 				if (this.item.images.poster.enabled){
-					sources.push(`/media/${this.type}s/${directory}/${this.item.images.poster.filename} 132w`)
+					if (this.item.images.poster.files.length){
+						this.item.images.poster.files.forEach(file=>{
+							sources.push(`/media/${this.type}s/${directory}/${file.filename} ${file.width}w`)
+						})
+					} else {
+						sources.push(`/media/${this.type}s/${directory}/${this.item.images.poster.filename} 250w`)
+					}
 				} else {
-					sources.push(`/static/gfx/default-cover.png 132w`)
+					sources.push(`/static/gfx/default-cover.png 250w`)
 					this.title.text = this.item.title
 					this.title.poster = true
 				}
--- server/models/schema/show.js
+++ server/models/schema/show.js
@@ -39,16 +39,31 @@ const showSchema = new mongoose.Schema({
 	images: {
 		background: {
 			enabled: {type: Boolean, default: false},
+			files: [{
+				_id: false,
+				width: String,
+				filename: String
+			}],
 			filename: String,
 			source: String
 		},
 		banner: {
 			enabled: {type: Boolean, default: false},
+			files: [{
+				_id: false,
+				width: String,
+				filename: String
+			}],
 			filename: String,
 			source: String
 		},
 		poster: {
 			enabled: {type: Boolean, default: false},
+			files: [{
+				_id: false,
+				width: String,
+				filename: String
+			}],
 			filename: String,
 			source: String
 		}
@@ -281,67 +296,64 @@ showSchema.methods.getSubscribers = function(){
 }
 
 showSchema.methods.setArtwork = function(data){
-	let promises = []
-	
-	if (data.url){
-		let promise = new Promise((resolve,reject)=>{
-			let target = require('path').join(this.getDirectory(), `${data.type}` + require('path').extname(data.url))
-			helpers.files.download(data.url, target)
-				.then(()=>{
-					this.images[data.type] = {
-						enabled: true,
-						filename: require('path').basename(target),
-						source: data.url
-					}
-					resolve()
-				})
-				.catch(error=>{
-					console.error(error)
-					reject(error)
-				})
-		})
-		promises.push(promise)
-	}
-	
-	if (data.preview){
-		let promise = new Promise((resolve,reject)=>{
-			let target = require('path').join(this.getDirectory(), `${data.type}-sml` + require('path').extname(data.preview))
-			helpers.files.download(data.preview, target)
-				.then(()=>{
-					this.images[data.type] = {
-						enabled: true,
-						filename: require('path').basename(target),
-						source: data.url
-					}
-					resolve()
-				})
-				.catch(error=>{
-					console.error(error)
-					reject(error)
-				})
-		})
-		promises.push(promise)
-	}
-	
-	return Promise.all(promises)
-	
-	/*
 	return new Promise((resolve,reject)=>{
-		let target = require('path').join(this.getDirectory(), data.type + require('path').extname(data.url))
+		if (!data.url) return reject()
+		
+		let target = require('path').join(this.getDirectory(), `${data.type}-original` + require('path').extname(data.url))
 		helpers.files.download(data.url, target)
 			.then(()=>{
+				// Resize image
+				let files = []
+				let sizes = []
+				
+				switch (data.type){
+					case 'poster':
+						sizes = [{
+							width: 250,
+							suffix: 'small'
+						},{
+							width: 500,
+							suffix: 'medium'
+						},{
+							width: 1000,
+							suffix: 'large'
+						}]
+						break
+					case 'banner':
+						sizes = [{
+							width: 575,
+							suffix: 'small'
+						},{
+							width: 800,
+							suffix: 'medium'
+						},{
+							width: 940,
+							suffix: 'large'
+						}]
+						break
+				}
+				
+				sizes.forEach(size=>{
+					let filename = target.replace(/-original/,`-${size.suffix}`)
+					require('sharp')(target).resize(size.width, null).toFile(filename)
+					files.push({
+						filename: require('path').basename(filename),
+						width: size.width
+					})
+				})
+				
 				this.images[data.type] = {
 					enabled: true,
-					filename: require('path').basename(target),
+					files: files,
 					source: data.url
 				}
 				resolve()
 			})
 			.catch(error=>{
+				console.error(error)
 				reject(error)
 			})
 	})
-	*/
 }
 showSchema.methods.setDirectory = function(){
 	// Use to rename an existing directory
@@ -459,6 +471,7 @@ showSchema.methods.sync = function(){
 				this.status = summary.status
 				this.synced = new Date(summary.updated_at)
 			}
+			
 			this.title = helpers.utils.normalize(summary.title)
 			this.overview = helpers.utils.normalize(summary.overview)
 			this.first_aired = new Date(summary.first_aired)
