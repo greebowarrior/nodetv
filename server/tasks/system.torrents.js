0ab335852f2646088953266a9f610f74b34c8502
Martin Purcell <martin.purcell@gmail.com>
2017-08-31 21:15:50 +0100

    Actually fixed regression

--- server/tasks/system.torrents.js
+++ server/tasks/system.torrents.js
@@ -10,50 +10,48 @@ require('node-schedule').scheduleJob('*/5 * * * *', ()=>{
 	console.debug('Checking for completed downloads')
 	
 	helpers.torrents.getComplete()
-		.then(torrents=>{
-			torrents.forEach(torrent=>{
-				Show.findByHashString(torrent.hashString)
-					.then(show=>{
-						if (!show) return null
-						
-						let idx = show.episodes.findIndex(item=>{
-							return item.file.download && item.file.download.hashString.toUpperCase() == torrent.hashString.toUpperCase()
+		.map(torrent=>{
+			Show.findByHashString(torrent.hashString)
+				.then(show=>{
+					if (!show) return null
+					
+					let idx = show.episodes.findIndex(item=>{
+						return item.file.download.hashString && item.file.download.hashString.toUpperCase() == torrent.hashString.toUpperCase()
+					})
+					
+					let episode  = show.episodes[idx]
+					
+					if (!episode.file.download.active) return null
+					
+					let files = torrent.files.filter(item=>{
+						return item.name.match(/\.(?:mkv|mp4|m4v)$/i)
+					})
+					
+					let directory = show.getDirectory()
+					let filename = episode.getFilename(files[0].name)
+					
+					if (!directory) throw new Error(`${show.title}: Directory not defined`)
+					
+					let source = require('path').join(torrent.downloadDir, files[0].name)
+					let target = require('path').join(directory, filename)
+					
+					return helpers.files.copy(source, target, show.config.transcode, episode)
+						.then(()=>{
+							return episode.setCollected(filename)
 						})
-						let episode  = show.episodes[idx]
-						
-						if (!episode.file.download.active) return null
-						
-						let files = torrent.files.filter(item=>{
-							return item.name.match(/\.(?:mkv|mp4|m4v)$/i)
+						.then(()=>{
+							return show.save()
 						})
-						
-						let directory = show.getDirectory()
-						let filename = episode.getFilename(files[0].name)
-						
-						if (!directory) throw new Error(`${show.title}: Directory not defined`)
-						
-						let source = require('path').join(torrent.downloadDir, files[0].name)
-						let target = require('path').join(directory, filename)
-						
-						return helpers.files.copy(source, target, show.config.transcode, episode)
-							.then(()=>{
-								return episode.setCollected(filename)
-							})
-							.then(()=>{
-								return show.save()
-							})
-					})
-					.then(()=>{
-						// Check seed ratio, if >= limit, remove torrent
-						if (torrent.uploadLimit >= torrent.seedRatioLimit || torrent.isFinished){
-							helpers.torrents.delete(torrent.id)
-						}
-					})
-					.catch(error=>{
-						console.error(error.message)
-					})
-			})
-		//	return Promise.all(promises)
+				})
+				.then(()=>{
+					// Check seed ratio, if >= limit, remove torrent
+					if (torrent.uploadLimit >= torrent.seedRatioLimit || torrent.isFinished){
+						helpers.torrents.delete(torrent.id)
+					}
+				})
+				.catch(error=>{
+					console.error(error.message)
+				})
 		})
 		.catch(error=>{
 			console.error(error.message)
